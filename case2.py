e43688b49c7cd22ba349fb4afeb4e5f006088c2d
zaidahmad23 <70536206+zaidahmad23@users.noreply.github.com>
2020-10-19 13:43:37 -0700

    Add files via upload

--- case2.py
+++ case2.py
@@ -1,21 +1,23 @@
 """
 Automate running of different scripts
 
-python3 case2.py -p0 0.2 -s 0.04 -n 4000 --nanc 100 --ton 100 --toff 0 \
+rm -rf output3 && DEBUG=1 python3 case2.py -p0 0.2 -s 0.04 -n 4000 --nanc 100 --ton 100 --toff 0 \
 --converted-filename relate_input \
---path-to-relate-bin ./relate_v1.1.2_x86_64_static/bin/Relate \
+--path-to-relate-bin /home/meow/Desktop/temp/relate_v1.1.2_x86_64_static/bin/Relate \
 --relate-output-filename relate_step_1 \
---relate-map-file-path clues/example/genetic_map.txt \
+--relate-map-file-path /home/meow/Desktop/temp/clues/example/genetic_map.txt \
 --inference-script-output-filename clues_output \
 --output-directory output3 --mutation-rate 1.25e-8 \
---create-ancient-samples --step2-script-ancient-samples-generation-gap 500 --step2-script-number-of-ancient-samples 100
+--create-ancient-samples --step2-script-ancient-samples-generation-gap 500 \
+--step2-script-number-of-ancient-samples 100 --runs 2
 """
-import subprocess
 import argparse
+import csv
 import os
-import sys
+import re
 import shutil
-
+import subprocess
+import sys
 from pathlib import Path
 
 import numpy as np
@@ -26,15 +28,35 @@ EXTERNAL_DEPENDENCIES = ["git", "gcc", "Rscript"]
 def is_debug_mode_active():
     return os.environ.get("DEBUG", False)
 
+
 def print_if_debug_mode_active(obj):
     if is_debug_mode_active():
         print(obj)
 
 
+def parse_inference_script_output_and_write_to_csv(writer, run, inference_script_output):
+    inference_script_output = inference_script_output.decode()
+    loglr, mle = None, [None, None]
+
+    match = re.search("logLR: (\d+.\d+)", inference_script_output)
+    if match:
+        loglr = match.group(1)
+
+    match = re.search("MLE:\s========\sepoch\s+selection\s(\S+\s*\S+)", inference_script_output)
+    if match:
+        mle = match.group(1).split()
+
+    writer.writerow({
+        "run #": run,
+        "logLR": loglr,
+        "epoch": mle[0],
+        "selection": mle[1]
+    })
+
 def execute_command(args, cwd=None):
     try:
         print_if_debug_mode_active(args)
-        subprocess.run(args, cwd=cwd, check=True, capture_output=True)
+        return subprocess.run(args, cwd=cwd, check=True, capture_output=True)
     except subprocess.CalledProcessError as err:
         stderr = err.stderr.decode("utf-8")
         stdout = err.stdout.decode("utf-8")
@@ -132,13 +154,15 @@ def convert_txt_to_haps_and_sample(r_script_path, input_txt_file_path, output_fi
     execute_command(f"Rscript {r_script_path} {input_txt_file_path} {output_file_name} 1000000 400".split(" "))
 
 
-def run_inference(ancient_samples_file_path, inference_output_filename):
+
+def run_inference(ancient_samples_file_path, inference_output_filename, time_bins):
     command = ["python3", "inference.py"]
     if ancient_samples_file_path is not None:
         command += ["--ancientSamps", ancient_samples_file_path]
-
+    if time_bins is not None:
+        command += ["--timeBins", time_bins]
     command += ["--out", inference_output_filename]
-    execute_command(command, cwd="./clues")
+    return execute_command(command, cwd="./clues").stdout
 
 
 def plot(mssel_traj_file_path, input_file_path, output_file_path, effective_population_size):
@@ -179,13 +203,14 @@ def ensure_internal_dependencies(args):
 
     for _file in required_files:
         if not os.path.isfile(_file):
-            print("Error: File {args.step_script_path} not found!")
+            print("Error: File {_file} not found!")
 
 
 def main():
     # Argument parsing/validation
 
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("--runs", type=int, default=1)
     argparser.add_argument("--output-directory", type=str, default='./')
     argparser.add_argument("--step-script-path", type=str, default="step.py", help="Path to step.py script")
     argparser.add_argument("--step-script-output-file-path", type=str, default="mssel.traj")
@@ -226,6 +251,7 @@ def main():
     argparser.add_argument("--converted-filename", type=str, help="Filename of converted .haps and .sample")
 
     argparser.add_argument("--inference-script-output-filename", type=str, required=True)
+    argparser.add_argument("--inference-script-time-bins-file-path", type=str, required=True)
     argparser.add_argument("--create-ancient-samples", action="store_true")
     argparser.add_argument("--step2-script-ancient-samples-generation-gap", type=str, required=True)
     argparser.add_argument("--step2-script-number-of-ancient-samples", type=int, required=True)
@@ -241,62 +267,75 @@ def main():
     args.output_directory = os.path.abspath(args.output_directory)
     os.makedirs(args.output_directory, exist_ok=True)
 
-    # Update output files
-    args.step_script_output_file_path = os.path.join(args.output_directory, args.step_script_output_file_path)
-    args.massel_output = os.path.join(args.output_directory, args.massel_output)
-    args.converted_filename = os.path.join(args.output_directory, args.converted_filename)
-    args.inference_script_output_filename = os.path.join(args.output_directory, args.inference_script_output_filename)
-    step2_script_ancient_samples_file_path = None
+    f = open(os.path.join(args.output_directory, "clues_output.csv"), "w")
+    writer = csv.DictWriter(f, fieldnames=["run #", "logLR", "epoch", "selection"])
+    writer.writeheader()
 
-    # Actual Computation
-    run_step(
-        p_initial=args.initial_allele_freq,
-        s=args.selection_coefficient,
-        n=args.effective_population_size,
-        output_file_path=args.step_script_output_file_path,
-        ton=args.ton,
-        toff=args.toff
-    )
-    run_mssel(
-        nchroms=args.nchroms,
-        nreps=args.nreps,
-        nder=args.nder,
-        nanc=args.nanc,
-        path_to_trajfile=args.step_script_output_file_path,
-        genome_length=args.genome_length,
-        sel_spot=args.sel_spot,
-        mutation_rate=args.mutation_rate_for_mssel,
-        recombination_rate=args.recombination_rate,
-        output_file=args.massel_output,
-    )
-    convert_txt_to_haps_and_sample(
-        r_script_path=args.path_to_converter_script,
-        input_txt_file_path=args.massel_output,
-        output_file_name=args.converted_filename,
-    )
+    original_output_directory = str(args.output_directory)
+    for n_run in range(1, args.runs + 1):
+        args.output_directory = os.path.join(original_output_directory, f"run_{n_run}")
+        os.makedirs(args.output_directory, exist_ok=True)
 
-    if args.create_ancient_samples:
-        step2_script_ancient_samples_file_path = os.path.join(args.output_directory, "ancientSamples.txt")
-        run_step2(
+        # Update output files
+        step_script_output_file_path = os.path.join(args.output_directory, args.step_script_output_file_path)
+        massel_output = os.path.join(args.output_directory, args.massel_output)
+        converted_filename = os.path.join(args.output_directory, args.converted_filename)
+        inference_script_output_filename = os.path.join(args.output_directory, args.inference_script_output_filename)
+
+        step2_script_ancient_samples_file_path = None
+
+        # Actual Computation
+        run_step(
             p_initial=args.initial_allele_freq,
             s=args.selection_coefficient,
             n=args.effective_population_size,
+            output_file_path=step_script_output_file_path,
             ton=args.ton,
-            toff=args.toff,
-            ancient_sample_generation_gap=args.step2_script_ancient_samples_generation_gap,
-            number_of_ancient_samples=args.step2_script_number_of_ancient_samples,
-            output_file_path=step2_script_ancient_samples_file_path
+            toff=args.toff
         )
-    run_inference(
-        ancient_samples_file_path=step2_script_ancient_samples_file_path,
-        inference_output_filename=args.inference_script_output_filename,
-    )
-    plot(
-        mssel_traj_file_path=args.step_script_output_file_path,
-        input_file_path=args.inference_script_output_filename,
-        output_file_path=os.path.join(args.output_directory, "plot"),
-        effective_population_size=args.effective_population_size
-    )
+        run_mssel(
+            nchroms=args.nchroms,
+            nreps=args.nreps,
+            nder=args.nder,
+            nanc=args.nanc,
+            path_to_trajfile=step_script_output_file_path,
+            genome_length=args.genome_length,
+            sel_spot=args.sel_spot,
+            mutation_rate=args.mutation_rate_for_mssel,
+            recombination_rate=args.recombination_rate,
+            output_file=massel_output,
+        )
+        convert_txt_to_haps_and_sample(
+            r_script_path=args.path_to_converter_script,
+            input_txt_file_path=massel_output,
+            output_file_name=converted_filename,
+        )
+
+        if args.create_ancient_samples:
+            step2_script_ancient_samples_file_path = os.path.join(args.output_directory, "ancientSamples.txt")
+            run_step2(
+                p_initial=args.initial_allele_freq,
+                s=args.selection_coefficient,
+                n=args.effective_population_size,
+                ton=args.ton,
+                toff=args.toff,
+                ancient_sample_generation_gap=args.step2_script_ancient_samples_generation_gap,
+                number_of_ancient_samples=args.step2_script_number_of_ancient_samples,
+                output_file_path=step2_script_ancient_samples_file_path
+            )
+        inference_script_output = run_inference(
+            ancient_samples_file_path=step2_script_ancient_samples_file_path,
+            inference_output_filename=inference_script_output_filename,
+            time_bins=args.inference_script_time_bins_file_path
+        )
+        parse_inference_script_output_and_write_to_csv(writer, n_run, inference_script_output)
+
+        plot(
+            mssel_traj_file_path=step_script_output_file_path,
+            input_file_path=inference_script_output_filename,
+            output_file_path=os.path.join(args.output_directory, "plot"),
+            effective_population_size=args.effective_population_size
+        )
 
 if __name__ == "__main__":
     main()
